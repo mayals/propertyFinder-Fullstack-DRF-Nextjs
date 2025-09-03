b851ba7e74321d4fae2fc06dee6f1b41313d52e0
May Al Sanea <developer.web.dj@gmail.com>
2025-09-04 00:30:08 +0300

    fix error in filter design

--- frontend/my_app/src/app/page.tsx
+++ frontend/my_app/src/app/page.tsx
@@ -2,16 +2,40 @@
 // src/app/page.tsx
 
 "use client";
+import { Elsie } from "next/font/google";
 import React, { useState, useEffect, useRef } from "react";
 import { FiChevronDown } from "react-icons/fi";
 
 export default function HomePage() {
+  const [activeRadioButton, setActiveRadioButton] = useState<string | null>(null);
+  // const [buyrentAction, setBuyrentAction] = useState<string | null>(null);
+  // const [commercialAction, setCommercialAction] = useState<string | null>(null);
   const [activeMenu, setActiveMenu] = useState<string | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
+
+
+  // buy button - rent button - comercial button
+  const handleactiveRadioButton = (buttonName: string) =>{
+    setActiveRadioButton(buttonName)
+  }
+  
+  // useEffect(() => {
+  //   if (activeRadioButton === "buy" || activeRadioButton === "rent"){
+  //       setBuyrentAction("buyRent_action");
+  //   }else if(activeRadioButton === "commercial"){
+  //       setCommercialAction("commercial_action");
+  //   }
+  // }, [activeRadioButton]);
+
+
+
+  //  property type menue - beds&bath menue 
   const toggleMenu = (menu: string) => {
     setActiveMenu((prev) => (prev === menu ? null : menu));
   };
+
+
 
   // close on click outside
   useEffect(() => {
@@ -24,6 +48,8 @@ export default function HomePage() {
     return () => document.removeEventListener("mousedown", onDown);
   }, []);
 
+
+
   // close on Esc
   useEffect(() => {
     const onKey = (e: KeyboardEvent) => {
@@ -32,6 +58,11 @@ export default function HomePage() {
     window.addEventListener("keydown", onKey);
     return () => window.removeEventListener("keydown", onKey);
   }, []);
+
+
+
+
+
 
   return (
     <section className="relative min-h-[450px] bg-cover bg-center bg-[url('https://static-assets.propertyfinder.com/images/homepage/hero/sa-desktop.jpg')]">
@@ -50,24 +81,48 @@ export default function HomePage() {
       >
         {/* Toggle buttons group */}
         <div className="flex items-center justify-center max-w-xl mx-auto mb-4">
-          <button
-            type="button"
+          
+          {/* buy */}
+          <input
+            type="radio"
+            name="options"
+            id="buy" 
+            onChange={() => handleactiveRadioButton("buy")}
+            checked={activeRadioButton === "buy"}
+          />
+          <label 
             className="w-full border-l border-t border-b text-base font-medium rounded-l-2xl text-black bg-white hover:bg-gray-100 px-4 py-1"
-          >
+            htmlFor="rent">
             Buy
-          </button>
-          <button
-            type="button"
-            className="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-1"
-          >
-            Rent
-          </button>
-          <button
-            type="button"
-            className="w-full border-t border-b border-r text-base font-medium rounded-r-2xl text-black bg-white hover:bg-gray-100 px-4 py-1"
-          >
-            Commercial
-          </button>
+          </label>
+            
+          {/* rent */}
+          <input
+            type="radio"
+            name="options"
+            id="rent"
+            onChange={() => handleactiveRadioButton("rent")}
+            checked={activeRadioButton === "rent"}
+          />
+          <label 
+              className="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-1"
+              htmlFor="rent">
+              Rent
+          </label>
+          
+          {/* commercial */}
+          <input
+            type="radio"
+            name="options"
+            id="commercial"
+            onChange={() => handleactiveRadioButton("commercial")}
+            checked={activeRadioButton === "commercial"}
+          />
+          <label 
+              className="w-full border-t border-b border-r text-base font-medium rounded-r-2xl text-black bg-white hover:bg-gray-100 px-4 py-1"
+              htmlFor="commercial">
+              Commercial
+          </label>
         </div>
 
         {/* Search input + dropdowns */}
@@ -94,7 +149,7 @@ export default function HomePage() {
               />
             </button>
 
-            {activeMenu === "property_type" && (
+            {activeMenu === "property_type" && activeRadioButton !== "commercial" && (
               <ul className="absolute left-0 top-full mt-1 w-52 bg-white border rounded-lg shadow-lg z-50">
                 <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Apartment</li>
                 <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Villa</li>
@@ -109,6 +164,26 @@ export default function HomePage() {
                 <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Full Floor</li>
               </ul>
             )}
+
+            {activeMenu === "property_type" && activeRadioButton === "commercial" && (
+              <ul className="absolute left-0 top-full mt-1 w-52 bg-white border rounded-lg shadow-lg z-50">
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Office Space</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Retail</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Warehouse</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Villa</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Show Room</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Bulk Units</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Factory</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                  Hotel / Hotel Apartment
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Labor Camp</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Staff Acomodation</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Shop</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Land</li>
+              </ul>
+            )}
+
           </div>
 
           {/* Beds & Baths dropdown */}
@@ -126,7 +201,7 @@ export default function HomePage() {
               />
             </button>
 
-            {activeMenu === "bed_bath" && (
+            {activeMenu === "bed_bath" && activeRadioButton !== "commercial" &&  (
               <ul className="absolute left-0 top-full mt-1 w-56 bg-white border rounded-lg shadow-lg z-50">
                 <li className="px-4 py-2 text-xs font-semibold text-gray-500 uppercase">
                   Bedrooms
@@ -150,8 +225,14 @@ export default function HomePage() {
             )}
           </div>
 
+          
+          
+          
+          
+          
+          
           {/* Search button */}
-          <button className="px-6 py-3 bg-[#ea3934] text-white font-semibold hover:bg-[#97211e] transition">
+          <button className="px-6 py-3 rounded-r-3xl bg-[#ea3934] text-white font-semibold hover:bg-[#97211e] transition">
             Search
           </button>
         </div>
