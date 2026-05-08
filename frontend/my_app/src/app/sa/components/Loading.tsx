68396881c740482e74eaa3ffe092ec07afbda933
May Al Sanea <developer.web.dj@gmail.com>
2026-05-09 01:33:32 +0400

    fix errors in loading

--- frontend/my_app/src/app/sa/components/Loading.tsx
+++ frontend/my_app/src/app/sa/components/Loading.tsx
@@ -4,14 +4,8 @@
 const Loading = () => {
    
     return (
-        <section> 
-            <div className="flex items-center justify-center h-screen">
-                <div className="relative">
-                    <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-                        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-[#ef5e4e] animate-spin">
-                    </div>
-                </div>
-            </div>
+        <section className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 flex items-center justify-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
         </section>
     )
 }
